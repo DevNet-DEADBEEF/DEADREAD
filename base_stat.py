dd6f73fd3f35629d3ccf6ef60698892dc5775d1a
PilotLep <pilot1782@pilot1782.org>
2025-09-25 18:53:04 -0600

    python implementation to base stats off of

---
+++ base_stat.py
@@ -0,0 +1,77 @@
+import sys
+import re
+
+file = sys.argv[1]
+debug = "--debug" in sys.argv
+
+# Ebook format
+"""
+... preamble ...
+...
+*** START OF THIS PROJECT GUTENBERG EBOOK <BOOK TITLE HERE> ***
+<ebook here>
+*** END OF THIS PROJECT GUTENBERG EBOOK <BOOK TITLE HERE> ***
+... end matter ...
+...
+"""
+
+sentence_end_chars = r"[.!?]+"
+word_deilmiters = r"\s+"
+
+def clean_word(word):
+    clean = re.sub(r'^[^a-z0-9-\']+$', '', word).lower()
+    if clean in ["", "-", "'"]:
+        return None
+    return clean
+
+with open(file, 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+    in_ebook = False
+    sentence_count = 0
+    sentence_len_sum = 0
+    word_freq = {}
+    title = "Unknown Title"
+    sentence_buffer = ""
+
+    start_line = 0
+    end_line = -1
+
+    for i, line in enumerate(lines):
+        line = line.strip()
+        if line.startswith("*** START OF THIS PROJECT GUTENBERG EBOOK"):
+            in_ebook = True
+            title = line.split("EBOOK")[-1].strip().strip('*').strip()
+            start_line = i + 1
+            continue
+        elif line.startswith("*** END OF THIS PROJECT GUTENBERG EBOOK"):
+            end_line = i
+            break
+
+    ebook = " ".join(lines[start_line:end_line]).strip()
+
+    sentences = re.split(sentence_end_chars, ebook)
+    for sentence in sentences:
+        sentence = sentence.strip()
+        if not sentence:
+            continue
+        sentence_count += 1
+        words = re.split(word_deilmiters , sentence)
+        words = [word.lower() for word in words if word]
+        # print(words)
+        sentence_len_sum += len(words)
+        for word in words:
+            word = clean_word(word)
+            if not word:
+                continue
+            if word in word_freq:
+                word_freq[word] += 1
+            else:
+                word_freq[word] = 1
+
+    avg_sentence_len = sentence_len_sum / sentence_count if sentence_count > 0 else 0
+    print(title, "\\ ", end="")
+    print(avg_sentence_len, "\\ ", end="")
+    top_five = sorted(word_freq.items(), key=lambda x: x[1], reverse=True)[:5]
+    print(" ".join(word for word, freq in top_five))
+
+    print(sentence_count)
